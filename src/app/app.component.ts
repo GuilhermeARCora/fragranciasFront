b45d622e89467d2e6c6788ecf913eae2dbac72c2
Guilherme Corá <guilhermearcora@gmail.com>
2025-05-29 18:50:04 -0300

    Initial commit: Angular 19 project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'fragancias';
+    showAlert() {
+    Swal.fire({
+      title: 'Success!',
+      text: 'SweetAlert2 is working!',
+      icon: 'success',
+      confirmButtonText: 'Cool 😎'
+    });
+  }
 }
