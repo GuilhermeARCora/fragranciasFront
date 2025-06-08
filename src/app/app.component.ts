6c9977ff31e074ed7ee7427aa1c886acec8bdd2b
Guilherme Corá <guilhermearcora@gmail.com>
2025-06-08 08:31:31 -0300

    refactor() cleaned some files to start fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fragancias';
-    showAlert() {
-    Swal.fire({
-      title: 'Success!',
-      text: 'SweetAlert2 is working!',
-      icon: 'success',
-      confirmButtonText: 'Cool 😎'
-    });
-  }
-}
+
+};
