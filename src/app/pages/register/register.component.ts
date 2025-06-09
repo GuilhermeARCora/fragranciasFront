485dea15ca0519607df550b0a555c7d3dec0537d
Guilherme Corá <guilhermearcora@gmail.com>
2025-06-08 21:16:08 -0300

    feat(register) add start of register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { matchFieldsValidator } from '../../shared/validators/matchFields.validator';
+import { hasFormError } from '../../shared/utils/helpers';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
-}
+    registerForm!: FormGroup;
+    hasFormError = hasFormError;
+
+    ngOnInit(): void {
+        this.buildForm();
+    };
+
+    buildForm(){
+
+      this.registerForm = new FormGroup({
+            name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]),
+            email: new FormControl('',[Validators.required, Validators.email]),
+            password: new FormControl('',[Validators.required, Validators.minLength(6)]),
+            confirmPassword: new FormControl('',[Validators.required, Validators.minLength(6)]),
+            confirmEmail: new FormControl('',[Validators.required, Validators.email]),
+        },
+        {
+            validators: [
+                  matchFieldsValidator('email', 'confirmEmail'),
+                  matchFieldsValidator('password', 'confirmPassword'),
+            ]
+        }
+      );
+    };
+
+    onSubmit(){
+      console.log('Testing', this.registerForm.value);
+    }
+
+
+};
--- src/app/shared/validators/matchFields.validator.ts
+++ src/app/shared/validators/matchFields.validator.ts
@@ -1,34 +1,25 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function matchFieldsValidator(field1: string, field2: string): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const value1 = control.get(field1)?.value;
-    const value2 = control.get(field2)?.value;
+  return (group: AbstractControl): ValidationErrors | null => {
+    const control1 = group.get(field1);
+    const control2 = group.get(field2);
+
+    if (!control1 || !control2) return null;
+
+    const value1 = control1.value;
+    const value2 = control2.value;
 
     if (value1 !== value2) {
-      control.get(field2)?.setErrors({ notMatching: true });
-      return { notMatching: true };
-    } else {
-      // Clear the error if previously set
-      if (control.get(field2)?.hasError('notMatching')) {
-        control.get(field2)?.setErrors(null);
-      }
-      return null;
+      control2.setErrors({ matchFieldsValidator: true });
+      return { matchFieldsValidator: true }; // optional: return error on the group too
     }
+
+    // Clear error if previously set
+    if (control2.hasError('matchFieldsValidator')) {
+      control2.setErrors(null);
+    }
+
+    return null;
   };
 }
-
-
-/*
-
-{
-form Group
-},
-{
-validators: [
-      matchFieldsValidator('email', 'emailConfirm'),
-      matchFieldsValidator('password', 'passwordConfirm'),
-    ]
-}
-
-*/
