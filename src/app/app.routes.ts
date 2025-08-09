7af2ff8147a5a33add3f3fad1e8cf78f7155c33f
Guilherme Corá <guilhermearcora@gmail.com>
2025-08-09 02:01:11 -0300

    necessary restart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,12 @@
 import { Routes } from '@angular/router';
 
-import { AuthGuard } from './core/guards/auth.guard';
-import { RoleGuard } from './core/guards/role.guard';
-
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-import { ChangePasswordComponent } from './pages/change-password/change-password.component';
-import { AdminHomeComponent } from './pages/admin/admin-home/admin-home.component';
-import { IndividualProductComponent } from './pages/individual-product/individual-product.component';
-
-const protectedRoutes:Routes = [
-  { path: 'userProfile/:id', component: UserProfileComponent, canActivate: [AuthGuard] },
-  { path: 'passwordChange', component: ChangePasswordComponent, canActivate: [AuthGuard] },
-];
 
 const adminRoutes:Routes = [
-  {
-    path:'adminHome',
-    loadComponent: () => import('./pages/admin/admin-home/admin-home.component')
-    .then(c => c.AdminHomeComponent),
-    canActivate:[RoleGuard],
-    canActivateChild: [RoleGuard],
-    data:{roles: ['admin','master']},
-    children: [
-        {
-          path:'createEditProduct/:id',
-          data: { roles: ['admin', 'master'] },
-          loadComponent: () => import('./pages/admin/create-and-edit-product/create-and-edit-product.component')
-          .then(c => c.CreateAndEditProductComponent)
-        }
-    ]
-  }
+
 ];
 
 export const routes: Routes = [
@@ -50,7 +23,6 @@ export const routes: Routes = [
   },
 
   // Protected routes
-  ...protectedRoutes,
   ...adminRoutes,
 
   // 404 route
