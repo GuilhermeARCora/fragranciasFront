e6bcdd628f9cd0cc0e56a090fb4b43fcb6804683
Guilherme Corá <guilhermearcora@gmail.com>
2025-11-09 07:40:27 -0300

    hotfix: setting image max width in individual products pages, also another attempt on fixing the swiper slides in the home page

--- src/app/pages/home/list-of-products/list-of-products.component.ts
+++ src/app/pages/home/list-of-products/list-of-products.component.ts
@@ -1,9 +1,12 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, Input } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DestroyRef, inject, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from '../../../core/services/products/products.service';
 import { ProductCardComponent } from '../../../shared/components/product-card/product-card.component';
+import type { AfterViewInit, ElementRef } from '@angular/core';
 import type { Product } from '../../../shared/types/product';
-import type { Observable } from 'rxjs';
+import { debounceTime, tap, timer, type Observable } from 'rxjs';
+import type { SwiperContainer } from 'swiper/element';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list-of-products',
@@ -15,9 +18,35 @@ import type { Observable } from 'rxjs';
   styleUrl: './list-of-products.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class ListOfProductsComponent {
+export class ListOfProductsComponent implements AfterViewInit {
 
-  productService = inject(ProductsService);
+  @ViewChild('swiperEl') swiperEl!: ElementRef<SwiperContainer>;
   @Input({ required:true }) products$!: Observable<Product[]>;
+  productService = inject(ProductsService);
+  destroyRef = inject(DestroyRef);
+
+  ngAfterViewInit(): void {
+    // Delay reativo para garantir renderização estável
+    timer(400)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.forceUpdate());
+
+    // Quando os produtos chegam, refaz update com debounce
+    this.products$
+      .pipe(
+        debounceTime(250),
+        tap(() => timer(150).subscribe(() => this.forceUpdate())),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
+  }
+
+  forceUpdate(): void {
+    const swiper = this.swiperEl?.nativeElement?.swiper;
+    if (!swiper) return;
+
+    swiper.update();
+    swiper.slideTo(0);
+  }
 
 };
